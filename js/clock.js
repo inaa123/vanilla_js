8d1f77791ab60be8658bdaecf4532785f29ce9a7
inaa123 <dlsdk7912@naver.com>
2023-08-16 23:53:39 +0900

    0816

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,12 @@ const clock = document.querySelector("h2#clock"); //h2와id같이 사용o, id만
                                                 //clock 디폴트 로 00시 00분 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()} :${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+getClock(); //getClock을 먼저 호출하지 않으면 디폴트로 정해 둔 00:00:00이 보이고나서 시간이 갱신됨. getClcok()먼저 호출하면 바로 시작.
 setInterval(getClock, 1000);  //5초마다 sayHello함수 호출
 //setTimeout(getClock, 5000); // 5초 후에 sayHello함수 호출
